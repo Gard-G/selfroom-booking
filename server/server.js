ad51e92b06b3a302af81d54fb21afe6715ad97f9
Gard-G <tanapot-s@rmutp.ac.th>
2024-09-24 15:28:58 +0700

    Revert "เพิ่มสำหรับ Docker"
    This reverts commit 254454ec2f37d6e1f68692b85a1dfd772375f70d.

--- server/server.js
+++ server/server.js
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use('/images', express.static('public/images'));
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'booking_selfroomdb'
 });
 
 connection.connect(error => {
